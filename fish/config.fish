454d18280ee20cacd4c339e9a6d4a140451fd850
Owen Lusis <owen00064@gmail.com>
2026-03-25 12:36:11 +1100

    add fish abbrs

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     abbr --add nixconf micro ~/.config/nixos/conf.nix
     abbr --add nixbuild bash ~/.config/nixos/update.sh
     abbr --add nixpush git -C ~/.config/nixos push
-    abbr --add nixlint nixfmt --indent=4 .config/nixos/conf.nix
-    abbr --add nixgit git -C ~/.config/nixos
+    abbr --add nixlint nixfmt .config/nixos/conf.nix
+    abbr --add nixgit git -C ~/.config
+
+    abbr --add exitup sudo tailscale set --exit-node=home-server
+    abbr --add exitdown sudo tailscale set --exit-node=
 end
