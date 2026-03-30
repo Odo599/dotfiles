028ed26fe192d35608669f8452cd46814d2f70d8
Owen Lusis <owen00064@gmail.com>
2026-03-31 09:04:48 +1100

    nixos: update update.sh

--- nixos/update.sh
+++ nixos/update.sh
@@ -2,16 +2,19 @@
 set -e
 
 echo "=== UPDATING SYSTEM ==="
+echo "> sudo nixos-rebuild switch --flake ~/.config/nixos#nixos --impure"
 sudo nixos-rebuild switch --flake ~/.config/nixos#nixos --impure
 
 echo "=== FORMATTING ==="
+echo "> nixfmt ~/.config/nixos/configuration.nix"
 nixfmt ~/.config/nixos/configuration.nix
 
 echo "=== COMMITING TO GIT ==="
 rev=$(readlink /nix/var/nix/profiles/system | grep -o "[0-9]*")
 echo "Commiting revision $rev"
-wd=$(pwd)
+echo "> cd ~/.config/nixos"
 cd ~/.config/nixos
-git add configuration.nix flake.nix flake.lock home.nix
+echo "> git add configuration.nix flake.nix flake.lock home.nix ../hypr/hyprland.conf ../hypr/hyprland.nix"
+git add configuration.nix flake.nix flake.lock home.nix ../hypr/hyprland.conf ../hypr/hyprland.nix
+echo '> git commit -m "nixos: $rev"'
 git commit -m "nixos: $rev"
-cd $wd
