21b4899f875ec87bb1b37fb5b8a46657d450ce79
Your Name <owen00064@gmail.com>
2026-04-15 13:13:49 +1000

    nvim: add neogit

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -201,5 +201,28 @@ require("lazy").setup({
                 desc = "Quickfix List (Trouble)",
             },
         },
+    },
+    {
+        "esmuellert/codediff.nvim",
+        cmd = "CodeDiff",
+        opts = {
+            diff = {
+                layout = "inline"
+            }
+        }
+    },
+    {
+        "NeogitOrg/neogit",
+        lazy = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "esmuellert/codediff.nvim",
+            "m00qek/baleia.nvim",
+            "nvim-telescope/telescope.nvim"
+        },
+        cmd = "Neogit",
+        keys = {
+            { "<leader>gg", "<cmd>Neogit<cr>", desc="Show Neogit UI" }
+        }
     }
 })
