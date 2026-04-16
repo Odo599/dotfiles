bacee77ec021c566e832755d2c404a1c13e4d86b
Your Name <owen00064@gmail.com>
2026-04-16 19:29:23 +1000

    nvim: open neogit in floating window

--- nvim/lua/config/binds.lua
+++ nvim/lua/config/binds.lua
@@ -1,6 +1,13 @@
 local telescope_builtin = require("telescope.builtin")
+local neogit = require("neogit")
+
 vim.keymap.set("n","<leader>ff", telescope_builtin.find_files)
 vim.keymap.set("n","<leader>bb", telescope_builtin.buffers)
 
 vim.keymap.set("n","<leader>jj", "<cmd>HopWord<cr>")
-vim.keymap.set("n","<leader>gg", "<cmd>Neogit<cr>")
+
+vim.keymap.set(
+    "n",
+    "<leader>gg", 
+    function() neogit.open({kind = "floating"}) end
+)
