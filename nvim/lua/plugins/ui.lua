b747beaddcade8e9fe41f248cb1630bf0eca1a2d
Owen Lusis <owen00064@gmail.com>
2026-04-18 13:28:17 +1000

    nvim: removed config option from start screen

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -19,8 +19,18 @@ return {
         "goolord/alpha-nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            local startify = require("alpha.themes.theta")
-            require("alpha").setup(startify.config)
+            local theta = require("alpha.themes.theta")
+            local dashboard = require("alpha.themes.dashboard")
+            theta.buttons.val = {
+                { type = "text",    val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+                { type = "padding", val = 1 },
+                dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+                dashboard.button("_ f f", "󰈞  Find file"),
+                dashboard.button("_ f g", "󰊄  Live grep"),
+                dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
+                dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+            },
+            require("alpha").setup(theta.config)
         end,
     }
 }
