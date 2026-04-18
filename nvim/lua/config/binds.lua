57f6c3e342f6bd9eee711389f81db982ce67088e
Owen Lusis <owen00064@gmail.com>
2026-04-18 11:41:31 +1000

    nvim: add keybind to show hidden files in telescope

--- nvim/lua/config/binds.lua
+++ nvim/lua/config/binds.lua
@@ -3,6 +3,7 @@ local neogit = require("neogit")
 local fterm = require("FTerm")
 
 vim.keymap.set("n","<leader>ff", telescope_builtin.find_files)
+vim.keymap.set("n","<leader>fh", "<cmd>Telescope find_files no_ignore=true<cr>")
 vim.keymap.set("n","<leader>bb", telescope_builtin.buffers)
 
 vim.keymap.set("n","<leader>jj", "<cmd>HopWord<cr>")
