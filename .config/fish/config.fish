ae6af2079dadc7ac95fc2bbe2293efb8075a35fe
Owen Lusis <owen00064@gmail.com>
2026-04-24 12:16:12 +1000

    fish: add n and c keybinds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ set -g fish_transient_prompt 1
 set -g fish_key_bindings fish_vi_key_bindings
 
 if status is-interactive
+    abbr --add n nvim
+    abbr --add c cd
     abbr --add exitup sudo tailscale set --exit-node=home-server
     abbr --add exitdown sudo tailscale set --exit-node=
 
