805fb2bf7a1852e57ed537adc089771cd361d69d
Owen Lusis <owen00064@gmail.com>
2026-04-24 12:17:20 +1000

    fish: do not show mode prompt in historical messages

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,4 +1,5 @@
 function fish_mode_prompt --description 'Displays the current mode'
-    # To reuse the mode indicator use this function instead
-    fish_default_mode_prompt
+    if ! contains -- --final-rendering $argv 
+        fish_default_mode_prompt
+    end
 end
